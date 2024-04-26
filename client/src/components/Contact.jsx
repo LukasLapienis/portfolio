820a4c4ef57f591edabceae016970b4918d20de2
LukasLapienis <lapienislukasp@gmail.com>
2024-04-26 20:36:32 +0300

    update

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -2,7 +2,7 @@ import { ContactForm } from './ContactForm';
 
 export const Contact = () => {
   return (
-    <section id="contact" className=" pt-24">
+    <section id="contact" className="flex flex-col gap-7 pt-24">
       <h3 className="text-teal-600 text-2xl pt-24">Contact</h3>
       <ContactForm />
     </section>
--- client/src/components/Skills.jsx
+++ client/src/components/Skills.jsx
@@ -26,7 +26,7 @@ export const Skills = () => {
   ];
 
   return (
-    <section id="skills" className="flex flex-col gap-4 pt-24">
+    <section id="skills" className="flex flex-col gap-7 pt-24">
       <h3 className="text-teal-600 text-2xl">Skills</h3>
       <div className="flex flex-wrap gap-4 items-center justify-center">
         {skills.map((skill) => {
